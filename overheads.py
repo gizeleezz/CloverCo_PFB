7c578561eac33b6614681b7df476786796757a44
taliajloh <taliaj.lzy@gmail.com>
2023-08-05 00:17:14 +0800

    added a few more comments to overhead code

--- overheads.py
+++ overheads.py
@@ -29,8 +29,11 @@ def highestOverheads(Overhead_expenses):
     for expense in Overhead_expenses:
     # Check to see if the current expense percentage is higher than the highest amount previously discovered
         if expense[1]> highest_overhead:
+        # Updating the highest overhead and the overhead category name that corresponds
             highest_overhead=expense[1]
             name_of_overhead=expense[0]
+    # Return function indicating the name of highest overhead and the percentage
     return f"[HIGHEST OVERHEAD]: {name_of_overhead}, {highest_overhead}%"
+# Run the highestOverheads function, then save the outcome
 result=highestOverheads(Overhead_expenses)
 print(result)
