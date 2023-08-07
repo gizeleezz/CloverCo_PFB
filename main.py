4b19c35628d693a4e6558b17ec93f0b7c93a4c09
gizeleezz <giselelollipop@gmail.com>
2023-08-07 14:06:49 +0800

    added main .py code

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-#add all functions
+from Cash_on_hand import analyse_cash_on_hand
+from overheads import highestOverheads
+from Profit_Loss import profit_and_loss
 
-import Cash_on_hand, overheads, Profit_Loss
+def main():
+    Overhead_expenses = ...  # Define Overhead_expenses appropriately
+    file_path = ...  # Define file_path appropriately
+
+    highestOverheads(Overhead_expenses)
+    analyse_cash_on_hand()
+    profit_and_loss(file_path)
+
+
+    main()
+
