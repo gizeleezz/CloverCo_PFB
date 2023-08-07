aaa2a98e512a24870229a18b547c21d10965c179
gizeleezz <giselelollipop@gmail.com>
2023-08-07 14:41:17 +0800

    added main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from overheads import highestOverheads
 from Profit_Loss import profit_and_loss
 
 def main():
-    Overhead_expenses = ...  # Define Overhead_expenses appropriately
-    file_path = ...  # Define file_path appropriately
+    # Define file_path appropriately in your code
+    subfolder_path = "csv_reports"  # Update if necessary
+    file_path_cash_on_hand = subfolder_path + "/Cash_on_Hand.csv"
+    file_path_profit_loss = subfolder_path + "/profit_and_loss(2).csv"
 
-    highestOverheads(Overhead_expenses)
-    analyse_cash_on_hand()
-    profit_and_loss(file_path)
-
+    # Call the functions directly with the defined file_path and other arguments
+    highestOverheads(file_path_profit_loss)  # Update forex accordingly
+    analyse_cash_on_hand(file_path_cash_on_hand)  # Update forex accordingly
+    profit_and_loss(file_path_profit_loss)  # Update forex accordingly
 
     main()
 
